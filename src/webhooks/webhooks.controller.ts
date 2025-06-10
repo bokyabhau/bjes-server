a419fb01e6cf9e45f86aecfde0108d77e79034fa
BokyaBhau <shivambhuadi@gmail.com>
2025-06-10 11:42:20 +0200

    register new webhook with meta cloud api

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { User } from 'src/users/user.schema';
 import { UsersService } from 'src/users/users.service';
 
@@ -14,9 +15,22 @@ export class WebhooksController {
   }
 
   @Get()
-  async getAllUsers(): Promise<User[]> {
+  async verifyAndSubscribe(@Query() query: any, @Res() res: Response): Promise<void> {
     // This method retrieves all users from the database
-    const users = await this.userService.findAll();
-    return users;
+    const mode = query['hub.mode'];
+    const token = query['hub.verify_token'];
+    const challenge = query['hub.challenge'];
+
+    console.log('Verification query:', query);
+
+    // check the mode and token sent are correct
+    if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
+      // respond with 200 OK and challenge token from the request
+      res.status(200).send(challenge);
+      console.log('Webhook verified successfully!');
+    } else {
+      // respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
 }
